9a1c51d9b7da44b84afd3e28b70a50713f21a31d
Roman Zaytsev <r.zaytsev@me.com>
2013-07-03 17:09:23 +0400

    bug fixes for pyton 2.6

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def get_today_sell_data(user, metal,today_sell_price):
     res = ' '
     r = redis.StrictRedis(host="localhost",port=6379,db=0)
     items = r.lrange('user:'+user+':items',0,-1)
-    if len(items) > 0:
+    if items:
         for item in items:
             i = item.split(',')
             if metal == int(i[0]):
